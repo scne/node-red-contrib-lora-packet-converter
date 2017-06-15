b731ce48bb5ef9f260e2f15d67288d39549417dc
Claudio Pomo <claudio.pomo@gmail.com>
2017-06-15 15:46:11 +0200

    get device code

--- lora-packet-converter/lora-packet-converter.js
+++ lora-packet-converter/lora-packet-converter.js
@@ -12,22 +12,28 @@ module.exports = function(RED) {
         var node = this;
 
         node.on('input', function(msg) {
-            var buf = new Buffer(splice(msg.payload, 0, 12)).toString('utf8');
-            var json = JSON.parse(buf);
-            if(json.rxpk !== undefined){
-                var packet = lora_packet.fromWire(new Buffer(json.rxpk[0].data, 'base64'));
+            if(msg.payload.length>12){
+                var bufferDevice = new Buffer(msg.payload.slice( 0, 11)).toString('hex');
 
-                var NwkSKey = new Buffer(config.nsw, 'hex');
-                if(lora_packet.verifyMIC(packet, NwkSKey)){
-                    var AppSKey = new Buffer(config.asw, 'hex');
-                    var decrypt = lora_packet.decrypt(packet, AppSKey, NwkSKey).toString();
-                    json.rxpk[0].data = decrypt;
-                    msg.payload = json;
-                    node.status({});
-                    node.send(msg);
+                var bufferPayload = Buffer.from(splice(msg.payload, 0, 12)).toString('utf8');
+                var json = JSON.parse(bufferPayload);
+                if(json.rxpk !== undefined){
+                    var packet = lora_packet.fromWire(new Buffer(json.rxpk[0].data, 'base64'));
 
+                    var NwkSKey = new Buffer(config.nsw, 'hex');
+                    if(lora_packet.verifyMIC(packet, NwkSKey)){
+                        var AppSKey = new Buffer(config.asw, 'hex');
+                        json.rxpk[0].data = lora_packet.decrypt(packet, AppSKey, NwkSKey).toString();
+                        json.rxpk[0].device = bufferDevice;
+                        msg.payload = json;
+                        node.status({});
+                        node.send(msg);
+
+                    } else {
+                        this.error("fail Network key for! Raw packet is " + packet );
+                        node.send(null);
+                    }
                 } else {
-                    node.error("hit an error", "fail Network key for "+msg);
                     node.send(null);
                 }
             } else {
